740a7380fe39d5bf856e631fa9a0f275d8fb0be7
Hyunook Kwon <hyunook23@gmail.com>
2022-11-23 02:42:20 -0500

    css and uikit

---
+++ staticfiles/js/main.js
@@ -0,0 +1,23 @@
+// jQuery to replace into shortened url string
+$(document).on("submit", "#shortener", function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/create/",
+        data: {
+            link: $("#link").val(),
+            csrfmiddlewaretoken: $("input[name=csrfmiddlewaretoken]").val(),
+        },
+        success: function (data) {
+            base_url = "http://127.0.0.1:8000/"
+            $("h2").html(base_url + data)
+        }
+
+    });
+});
+
+// clip the new url
+function ctrl_cv() {
+    let url_link = document.getElementById("output").innerHTML
+    navigator.clipboard.writeText(url_link);
+}
